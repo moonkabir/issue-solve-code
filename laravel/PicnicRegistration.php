c6d9767b8263c4c9f1ded5862a037bf330a67c8a
MoonKabir <45933210+moonkabir@users.noreply.github.com>
2023-02-08 14:58:52 +0600

    Laravel custom model

---
+++ laravel/PicnicRegistration.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class PicnicRegistration extends Model
+{
+    protected $guarded = ['id'];
+    protected $table = "picnic_registration";
+
+}
