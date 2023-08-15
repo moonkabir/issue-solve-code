32ea4505e8bef627f9cea666fe70b198bcc9f846
moonkabir <moonkabir4@gmail.com>
2023-08-15 09:33:06 +0600

    laravel feature added

---
+++ laravel/laravel_feature.php
@@ -0,0 +1,21 @@
+<?php
+
+// get all data from database using model
+$brands = App\Models\Lookup::all();
+
+// get data from database using model with id
+Product::find($products_id);
+
+// static function get from model 
+public static function moonkabir(){
+    return "moon kabir";
+}
+$moonkabir = App\Models\Lookup::moonkabir();
+echo $moonkabir;
+
+// function get from model 
+public function moon(){
+    return "moon";
+}
+$moon =  new App\Models\Lookup;
+echo $moon->moon();
